c52873aeb62e7e5618f24a2d493e15d9693a104d
Vansh Behal <99304983+Vbehal01@users.noreply.github.com>
2023-09-20 10:13:39 +0000

    model for db

--- store_monitoring/model.py
+++ store_monitoring/model.py
@@ -1,23 +1,31 @@
-from sqlalchemy import Column, Integer, String, TIMESTAMP, TIME
+from sqlalchemy import Column, Integer, String, TIMESTAMP, TIME, BigInteger
 from database import Base
 
-class Input_1(Base):
-    __tablename__="input_1"
+class StoreStatus(Base):
+    __tablename__="store_status"
 
-    store_id=Column(Integer, primary_key=True)
+    id=Column(Integer, primary_key=True, autoincrement=True)
+    store_id=Column(BigInteger)
     timestamp_utc=Column(TIMESTAMP)
     status=Column(String)
 
-class Input_2(Base):
-    __tablename__="input_2"
+    class StoreStatus:
+        def __init__(self, store_id, status, timestamp_utc):
+            self.store_id = store_id
+            self.status = status
+            self.timestamp_utc = timestamp_utc
 
-    store_id=Column(Integer, primary_key=True)
+class StoreHours(Base):
+    __tablename__="store_hours"
+
+    id=Column(Integer, primary_key=True, autoincrement=True)
+    store_id=Column(Integer)
     dayofweek=Column(Integer)
     start_time_local=Column(TIME)
     end_time_local=Column(TIME)
 
-class Input_3(Base):
-    __tablename__="input_3"
+class StoreTimeZone(Base):
+    __tablename__="store_timezone"
 
     store_id=Column(Integer, primary_key=True)
-    timezone_str=Column(String)
+    timezone_str=Column(String)                                                                                                                                                            
