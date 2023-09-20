437568377445283ad4b3afaa72e9cf53ca892e16
Vansh Behal <99304983+Vbehal01@users.noreply.github.com>
2023-09-20 13:08:42 +0000

    adding path of all 3 csv's

--- store_monitoring/constants.py
+++ store_monitoring/constants.py
@@ -1,1 +1,3 @@
-FILE_PATH="/workspaces/Store-monitoring/store_status.csv"
+STORE_STATUS_FILE_PATH="/workspaces/Store-monitoring/csv files/store_status.csv"
+MENU_HOURS_FILE_PATH="/workspaces/Store-monitoring/csv files/Menu hours.csv"
+BQ_RESULTS_FILE_PATH="/workspaces/Store-monitoring/csv files/Bq results.csv"
